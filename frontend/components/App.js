83d5ab627d8e2f7443dfeb439774cc107abb89ac
Bryant Clark <bryantc4110@yahoo.com>
2024-09-20 20:06:09 -0400

    almost done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import {Route, Routes, Link} from "react-router-dom"
 
 function App() {
   return (
     <div id="app">
       <nav>
-        {/* NavLinks here */}
+        <Link to ="/">Home</Link>
+        <Link to='/order'>Order</Link>
       </nav>
-      {/* Route and Routes here */}
-      <Home />
-      <Form />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route />
+      </Routes>
     </div>
   )
 }
